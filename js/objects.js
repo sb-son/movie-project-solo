0ca2b2cf90f80c57f0a8a2e4a2a1fc60b7f39c59
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-12-21 18:02:59 -0600

    added some style to cards. added popover. working on popover functionality

--- js/objects.js
+++ js/objects.js
@@ -11,10 +11,11 @@ const editBtn = "<a href='#' class='edit text-white bg-success rounded-3 align-i
 
 const modalBtn = `<button type="button" class="btn btn-primary" id="modal-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">+</button>`
 
-const submitForm = `<label for="title"> Add a movie
-\t<input type="text" placeholder="Enter Movie Title..." name="title" id="title">
+const submitForm = `<label for="title">
+\t<input type="text" placeholder="Enter Movie Title..." name="title" id="title" data-bs-toggle="popover" data-bs-content="Enter a movie">
 \t</label>
 \t<br>
+<div id="rating-popover" data-bs-toggle="popover" data-bs-content="Enter a rating">
 <fieldset class="rating">
 \t<input type="radio" id="1-star" name="star" class="star" value="5"
 \t\t>
@@ -31,21 +32,21 @@ const submitForm = `<label for="title"> Add a movie
 \t<input type="radio" id="5-star" name="star" class="star" value="1"
 \t\t>
 \t<label for="5-star">5</label>
-</fieldset>`
+</fieldset>
+</div>`
 
 const modalContent = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Enter a movie you would like to add</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         ${submitForm}
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary" id="submit-button">Understood</button>
+        <button type="submit" class="btn btn-primary" id="submit-button">Add</button>
       </div>
     </div>
   </div>
@@ -104,7 +105,6 @@ $(document).ready(() => {
 
 let movieContainer =[];
 
-//// movie catcher
 function getMovies() {
     movieContainer = [];
     fetch(`https://adaptive-dent-wasabi.glitch.me/movies`)
@@ -114,10 +114,10 @@ function getMovies() {
             $("#spinner").empty();
             data.forEach(function (m) {
                 movieContainer.push(new Movie(m.id, m.title, m.rating))
-                $("#movies").append(`<div class="card col-4">Title: ${m.title} Rating: ${m.rating} <span id="movie-id" style="display: none">${m.id}</span>${editBtn} ${deleteBtn}</div>`);
+                $("#movies").append(`<div class="card d-flex col-4 justify-content-between p-0">Title: ${m.title} Rating: ${m.rating} <span id="movie-id" style="display: none">${m.id}</span><div>${editBtn} ${deleteBtn}</div></div>`);
             })
-            $("#movies").append(`<div class="card col-4">${modalBtn} ${modalContent}</div>`)
 
+            $("#movies").append(`<div class="card col-4 justify-content-center align-items-center p-0">${modalBtn} ${modalContent}</div>`)
         })
         .then(() => {
             addMovie();
@@ -133,22 +133,44 @@ const addMovie = () => {
         let rating = $('input[name="star"]:checked').val();
         let movie = new Movie('', title, rating);
         if (title === '' && rating === undefined) {
-                    $("#title").css("border-color", "red")
-                    $("#title").attr("placeholder", "Enter a title")
+                    $("#title").css("border-color", "red");
+                    $("#title").attr("placeholder", "Enter a title");
+                    $("#rating-popover").popover("show");
+                    setTimeout(function () {
+                        $("#rating-popover").popover("hide");
+                    }, 5000);
+                    $("#title").popover("show");
                 } else if (title === '') {
                     $("#title").css("border-color", "red")
+                    $("#title").popover("show");
                 } else if (rating === undefined) {
                     console.log('need a rating')
+                    $("#rating-popover").popover("show");
+                    setTimeout(function () {
+                        $('.popover-body').fadeOut('slow');
+                        $("#rating-popover").popover("hide");
+                    }, 5000);
                 } else {
                     movie.add()
+                    $('#exampleModal').modal("hide");
                 }
+    })
+
+    $("#rating-popover").popover({
+        placement: 'bottom'
+    });
+
+    $(".btn-close").click(function () {
+        $("#title").val('');
+        $("#title").css("border-color", "");
+        $('input[name="star"]:checked').prop('checked', false)
     })
 }
 
 const deleteMovie = () => {
     $(".delete").click(function (e) {
         e.preventDefault();
-        let movieID = parseInt($(this).siblings("#movie-id").html())
+        let movieID = parseInt($(this).parent().siblings("#movie-id").html())
         for (let movie of movieContainer) {
             if (movie.id === movieID) {
                 movie.delete()
