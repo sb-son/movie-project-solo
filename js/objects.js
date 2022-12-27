a660fc6f04a9eb24e895d4cbcbb1ef369ec0cac8
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-12-27 13:50:17 -0600

    added edit modal

--- js/objects.js
+++ js/objects.js
@@ -4,10 +4,12 @@ const deleteBtn = "<a href='#' class='delete text-white bg-danger rounded-3 alig
     "  <path d=\"M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z\"/>\n" +
     "</svg>Delete</a>";
 
-const editBtn = "<a href='#' class='edit text-white bg-success rounded-3 align-items-center'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\">\n" +
-    "  <path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>\n" +
-    "  <path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>\n" +
-    "</svg>Edit</a>";
+// const editBtn = "<a href='#' class='edit text-white bg-success rounded-3 align-items-center'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\">\n" +
+//     "  <path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>\n" +
+//     "  <path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>\n" +
+//     "</svg>Edit</a>";
+
+const editBtn = `<button type="button" class="btn btn-primary" id="edit-modal-btn" data-bs-toggle="modal" data-bs-target="#editModalContent">Edit</button>`
 
 const cancelBtn = "<a href='#' class='cancel text-white bg-success rounded-3 align-items-center'>Cancel</a>";
 
@@ -47,6 +49,23 @@ const modalContent = `<div class="modal fade" id="exampleModal" tabindex="-1" ar
       </div>
       <div class="modal-footer">
         <button type="submit" class="btn btn-primary" id="submit-button">Add</button>
+      </div>
+    </div>
+  </div>
+</div>`
+
+const editModalContent = `<div class="modal fade" id="editModalContent" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit Movie</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ${submitForm}
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-primary" id="edit-button">Confirm</button>
       </div>
     </div>
   </div>
@@ -113,7 +132,7 @@ function getMovies() {
             $("#spinner").empty();
             data.forEach(function (m) {
                 movieContainer.push(new Movie(m.id, m.title, m.rating))
-                $("#movies").append(`<div class="card d-flex col-4 justify-content-between p-0"><div id="front" class="cardFront">Title: ${m.title} Rating: ${m.rating} <span id="movie-id" style="display: none">${m.id}</span><div>${editBtn}<button type="button" class="flip-btn" id="flip-btn">Flip Card</button></div></div><div id="back" class="cardBack">${deleteBtn} ${cancelBtn}</div></div>`);
+                $("#movies").append(`<div class="card d-flex col-4 justify-content-between p-0"><div id="front" class="cardFront">Title: ${m.title} Rating: ${m.rating} <span id="movie-id" style="display: none">${m.id}</span><div>${editBtn}<button type="button" class="flip-btn" id="flip-btn">Flip Card</button></div></div><div id="back" class="cardBack">${deleteBtn} ${cancelBtn}</div> ${editModalContent}</div>`);
             })
             $("#movies").append(`<div class="card col-4 justify-content-center align-items-center p-0">${modalBtn} ${modalContent}</div>`)
         })
@@ -181,6 +200,7 @@ const addMovie = () => {
 
     $('#exampleModal').on('hide.bs.modal', function () {
         $("#title").val('')
+        $("#title").css("border-color", "");
         $('input[name="star"]:checked').prop('checked', false)
     })
 
@@ -208,6 +228,7 @@ const deleteMovie = () => {
 }
 
 const editMovie = () => {
+    // let movieID = parseInt($(this).siblings("#movie-id").html())
     let movieID = parseInt($(this).siblings("#movie-id").html())
     $(".edit").click(function () {
 
